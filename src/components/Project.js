ec79c3841449ea2eb1db7815e4965d84904a0157
MD. Ariful Alam <swazan.arif@gmail.com>
2021-08-30 12:55:53 +0600

    Change title style

--- src/components/Project.js
+++ src/components/Project.js
@@ -17,7 +17,7 @@ const Project = () => {
                         <div>
                             <div className="flex items-center">
                                 <span>
-                                    <h5 className="card-title">
+                                    <h5 className="card-title text-lg">
                                         {skeleton({ width: 'w-32', height: 'h-8' })}
                                     </h5>
                                 </span>
@@ -77,7 +77,7 @@ const Project = () => {
                         <div className="flex items-center opacity-60">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 mr-2 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
                             <span>
-                                <h5 className="card-title">
+                                <h5 className="card-title text-lg">
                                     {item.name}
                                 </h5>
                             </span>
