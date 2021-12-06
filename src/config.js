f4cfcbbe5e3d1a15b806174705c86d4296e4d704
MD. Ariful Alam <swazan.arif@gmail.com>
2021-12-07 00:16:01 +0600

    Exclude `laravel-ecommerce` from projects

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ module.exports = {
         limit: 8, // How many projects to display.
         exclude: {
             forks: false, // Forked projects will not be displayed if set to true.
-            projects: [] // These projects will not be displayed. example: ['my-project1', 'my-project2']
+            projects: ['laravel-ecommerce'] // These projects will not be displayed. example: ['my-project1', 'my-project2']
         }
     },
     social: {
