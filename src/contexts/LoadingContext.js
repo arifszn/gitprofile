cf043dab4cfc8c3e7f4afcf6c60911e0bdb89fc8
Ariful Alam <swazan.arif@gmail.com>
2022-03-19 00:10:13 +0600

    Add contexts

---
+++ src/contexts/LoadingContext.js
@@ -0,0 +1,15 @@
+import { createContext, useState } from 'react';
+
+const initialValue = true;
+
+export const LoadingContext = createContext();
+
+export const LoadingProvider = (props) => {
+  const [loading, setLoading] = useState(initialValue);
+
+  return (
+    <LoadingContext.Provider value={[loading, setLoading]}>
+      {props.children}
+    </LoadingContext.Provider>
+  );
+};
