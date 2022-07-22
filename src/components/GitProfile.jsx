18570482dc481c39635b161959bd6bf4a0ead8c2
Thirumal Dharmalingam <d.thirumal@gmail.com>
2022-07-22 22:01:30 +0400

    Update GitProfile.jsx

--- src/components/GitProfile.jsx
+++ src/components/GitProfile.jsx
@@ -203,7 +203,7 @@ const GitProfile = ({ config }) => {
                 <div className="card compact bg-base-100 shadow">
                   <a
                     className="card-body"
-                    href="https://github.com/arifszn/gitprofile"
+                    href="https://github.com/thirumald/thirumald.github.io"
                     target="_blank"
                     rel="noreferrer"
                   >
