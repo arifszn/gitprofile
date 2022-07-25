98a81b269821a236c89dcb2446b1439eb875c38d
Thirumal Dharmalingam <d.thirumal@gmail.com>
2022-07-25 09:50:49 +0400

    Update gitprofile.config.js
    git repository size changed from 20 to 10

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -4,7 +4,7 @@ const config = {
   github: {
     username: 'thirumald', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 20, // How many projects to display.
+    limit: 10, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
