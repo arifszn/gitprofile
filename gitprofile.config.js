f457ddeff747a7a08ef304793c3e0d930d3db8f4
Michel Doré <22073531+micheldore@users.noreply.github.com>
2022-10-01 14:24:15 +0000

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -4,7 +4,7 @@ const config = {
   github: {
     username: 'micheldore', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
+    limit: 8, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
@@ -12,79 +12,70 @@ const config = {
   },
   social: {
     linkedin: 'micheldore',
+    twitter: '',
     facebook: '',
     instagram: '',
     dribbble: '',
     behance: '',
-    website: 'https://micheldore.github.io',
+    medium: '',
+    dev: '',
+    website: '',
     phone: '',
     email: 'michel@doredev.nl',
   },
-  skills: [
-    'PHP',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'NextJS',
-    'MySQL',
-    'Git',
-    'Docker',
-    'CSS',
-    'Tailwind',
-    'Bootstrap',
-  ],
+  skills: ['JavaScript', 'React.js'],
   experiences: [
-//     {
-//       company: 'Monstarlab Bangladesh',
-//       position: 'Backend Engineer II',
-//       from: 'September 2021',
-//       to: 'Present',
-//     },
+    {
+      company: 'Company name 1',
+      position: 'Software Engineer',
+      from: 'July 2019',
+      to: 'Present',
+    },
+    {
+      company: 'Company name 2',
+      position: 'Jr. Software Engineer',
+      from: 'January 2019',
+      to: ' June 2019',
+    },
   ],
   education: [
-//     {
-//       institution: 'American International University-Bangladesh',
-//       degree: 'Bachelor of Science',
-//       from: '2015',
-//       to: '2019',
-//     },
-//     {
-//       institution: 'Cantonment College, Jessore',
-//       degree: 'Higher Secondary Certificate (HSC)',
-//       from: '2012',
-//       to: '2014',
-//     },
-//     {
-//       institution: 'Chowgacha Shahadat Pilot High School',
-//       degree: 'Secondary School Certificate (SSC)',
-//       from: '2007',
-//       to: '2012',
-//     },
+    {
+      institution: 'Institution name 1',
+      degree: 'ABC',
+      from: '2015',
+      to: '2019',
+    },
+    {
+      institution: 'Institution name 2',
+      degree: 'XYZ',
+      from: '2012',
+      to: '2014',
+    },
   ],
   // Display blog posts from your medium or dev account. (Optional)
-//   blog: {
-//     source: 'dev', // medium | dev
-//     username: 'arifszn', // to hide blog section, keep it empty
-//     limit: 3, // How many posts to display. Max is 10.
-//   },
+  blog: {
+    source: 'dev', // medium | dev
+    username: 'arifszn', // to hide blog section, keep it empty
+    limit: 5, // How many posts to display. Max is 10.
+  },
   googleAnalytics: {
-    // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-    id: '', // Please remove this and use your own tag id or keep it empty
+    // GA3 tracking id/GA4 tag id
+    id: '', // UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   hotjar: {
-    id: '', //  Please remove this and use your own id or keep it empty
+    id: '',
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'corporate',
+    defaultTheme: 'light',
 
-    // Hides the switch in the navbar
+    // Hides the theme change switch
     // Useful if you want to support a single color mode
     disableSwitch: false,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
-    respectPrefersColorScheme: false,
+    respectPrefersColorScheme: true,
 
     // Hide the ring in Profile picture
     hideAvatarRing: false,
@@ -135,5 +126,3 @@ const config = {
     },
   },
 };
-
-export default config;
