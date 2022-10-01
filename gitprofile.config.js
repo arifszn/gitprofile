e12240ed94aeda78de5ad0fdc42062453e57e002
Michel Doré <22073531+micheldore@users.noreply.github.com>
2022-10-01 14:21:06 +0000

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'micheldore', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
@@ -11,80 +11,68 @@ const config = {
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
+    linkedin: 'micheldore',
     facebook: '',
     instagram: '',
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    website: 'https://arifszn.github.io',
+    website: 'https://micheldore.github.io',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'michel@doredev.nl',
   },
   skills: [
     'PHP',
-    'Laravel',
     'JavaScript',
     'React.js',
     'Node.js',
-    'Nest.js',
+    'NextJS',
     'MySQL',
     'Git',
     'Docker',
-    'PHPUnit',
     'CSS',
-    'Antd',
     'Tailwind',
     'Bootstrap',
   ],
   experiences: [
-    {
-      company: 'Monstarlab Bangladesh',
-      position: 'Backend Engineer II',
-      from: 'September 2021',
-      to: 'Present',
-    },
-    {
-      company: 'My Offer 360 Degree',
-      position: 'Web Application Developer',
-      from: 'July 2019',
-      to: 'August 2021',
-    },
+//     {
+//       company: 'Monstarlab Bangladesh',
+//       position: 'Backend Engineer II',
+//       from: 'September 2021',
+//       to: 'Present',
+//     },
   ],
   education: [
-    {
-      institution: 'American International University-Bangladesh',
-      degree: 'Bachelor of Science',
-      from: '2015',
-      to: '2019',
-    },
-    {
-      institution: 'Cantonment College, Jessore',
-      degree: 'Higher Secondary Certificate (HSC)',
-      from: '2012',
-      to: '2014',
-    },
-    {
-      institution: 'Chowgacha Shahadat Pilot High School',
-      degree: 'Secondary School Certificate (SSC)',
-      from: '2007',
-      to: '2012',
-    },
+//     {
+//       institution: 'American International University-Bangladesh',
+//       degree: 'Bachelor of Science',
+//       from: '2015',
+//       to: '2019',
+//     },
+//     {
+//       institution: 'Cantonment College, Jessore',
+//       degree: 'Higher Secondary Certificate (HSC)',
+//       from: '2012',
+//       to: '2014',
+//     },
+//     {
+//       institution: 'Chowgacha Shahadat Pilot High School',
+//       degree: 'Secondary School Certificate (SSC)',
+//       from: '2007',
+//       to: '2012',
+//     },
   ],
   // Display blog posts from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 3, // How many posts to display. Max is 10.
-  },
+//   blog: {
+//     source: 'dev', // medium | dev
+//     username: 'arifszn', // to hide blog section, keep it empty
+//     limit: 3, // How many posts to display. Max is 10.
+//   },
   googleAnalytics: {
     // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-    id: 'G-WLLB5E14M6', // Please remove this and use your own tag id or keep it empty
+    id: '', // Please remove this and use your own tag id or keep it empty
   },
   hotjar: {
-    id: '2617601', //  Please remove this and use your own id or keep it empty
+    id: '', //  Please remove this and use your own id or keep it empty
     snippetVersion: 6,
   },
   themeConfig: {
