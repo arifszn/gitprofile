89e5c29d806f6e320b55670f26936b5ba3dae0c4
Ariful Alam <ariful.alam@monstar-lab.com>
2022-10-25 18:07:06 +0600

    Handle empty name in meta tags

--- src/components/head-tag-editor/index.jsx
+++ src/components/head-tag-editor/index.jsx
@@ -22,7 +22,7 @@ const HeadTagEditor = ({ profile, theme, googleAnalytics, social }) => {
                 gtag('config', '${googleAnalytics.id}');`}
             </script>
           )}
-          <title>Portfolio of {profile.name}</title>
+          <title>Portfolio{profile.name && ` of ${profile.name}`}</title>
           <meta
             name="theme-color"
             content={isDarkishTheme(theme) ? '#000000' : '#ffffff'}
@@ -30,18 +30,27 @@ const HeadTagEditor = ({ profile, theme, googleAnalytics, social }) => {
 
           <meta name="description" content={profile.bio} />
 
-          <meta itemProp="name" content={`Portfolio of ${profile.name}`} />
+          <meta
+            itemProp="name"
+            content={`Portfolio${profile.name && ` of ${profile.name}`}`}
+          />
           <meta itemProp="description" content={profile.bio} />
           <meta itemProp="image" content={profile.avatar} />
 
           <meta property="og:url" content={social?.website || ''} />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content={`Portfolio of ${profile.name}`} />
+          <meta
+            property="og:title"
+            content={`Portfolio${profile.name && ` of ${profile.name}`}`}
+          />
           <meta property="og:description" content={profile.bio} />
           <meta property="og:image" content={profile.avatar} />
 
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content={`Portfolio of ${profile.name}`} />
+          <meta
+            name="twitter:title"
+            content={`Portfolio${profile.name && ` of ${profile.name}`}`}
+          />
           <meta name="twitter:description" content={profile.bio} />
           <meta name="twitter:image" content={profile.avatar} />
         </Helmet>
