203273f349f39d170ed7d2a2435256aa02676df8
Ariful Alam <ariful.alam@monstar-lab.com>
2023-01-03 18:13:16 +0600

    Reduce blog post limit

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -105,7 +105,7 @@ const config = {
   blog: {
     source: 'dev', // medium | dev
     username: 'arifszn', // to hide blog section, keep it empty
-    limit: 3, // How many posts to display. Max is 10.
+    limit: 2, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
