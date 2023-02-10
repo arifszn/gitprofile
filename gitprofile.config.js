e586b8a57d13345b30cfc921865b3e9035dab3ea
J K <me@jameskip.com>
2023-02-10 14:23:01 -0600

    forest

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -128,11 +128,11 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'forest',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
