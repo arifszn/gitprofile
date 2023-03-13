25da3b1a7a72554ccabd0be7b4e9d147f110398d
Somer Sinnard <93629653+somer-sinnard@users.noreply.github.com>
2023-03-13 18:25:00 -0500

    fix hide blog

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -105,7 +105,7 @@ const config = {
     //source: 'dev', // medium | dev
     //username: 'arifszn', // to hide blog section, keep it empty
     //limit: 2, // How many posts to display. Max is 10.
-  },
+  //},
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
