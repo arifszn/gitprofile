de7c57b6568d37a3d371c648476ae2c18ad24b59
Somer Sinnard <93629653+somer-sinnard@users.noreply.github.com>
2023-03-12 20:17:08 -0500

    no github projects

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -4,7 +4,7 @@ const config = {
   github: {
     username: 'somer-sinnard', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 5, // How many projects to display.
+    limit: 0, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
