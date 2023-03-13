97885e7f27bd4758129f013ff233f16f08694476
Somer Sinnard <93629653+somer-sinnard@users.noreply.github.com>
2023-03-13 18:09:32 -0500

    github profile section basic

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,11 +2,11 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'somer-sinnard', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
+    limit: 2, // How many projects to display.
     exclude: {
-      forks: false, // Forked projects will not be displayed if set to true.
+      forks: true, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
