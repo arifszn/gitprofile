08580f71ad93f4093ea5acf5adb86020e591aae9
Somer Sinnard <93629653+somer-sinnard@users.noreply.github.com>
2023-03-13 19:17:36 -0500

    cut repo

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -7,7 +7,7 @@ const config = {
     limit: 2, // How many projects to display.
     exclude: {
       forks: true, // Forked projects will not be displayed if set to true.
-      projects: ['Bio','-Hello_World-','MyFirstRepo','somer-sinnard.github.io_temp','HW12'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['Bio','-Hello_World-','MyFirstRepo','somer-sinnard.github.io_temp','HW12','Somer-Sinnard'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
