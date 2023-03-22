ec93a667de6eef5834b39c1cc12f8c79c26a2863
Alexander Carr <jalexandercarr@gmail.com>
2023-03-22 17:43:27 -0400

    Updated github list amount

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -4,7 +4,7 @@ const config = {
   github: {
     username: 'jalexandercarr', // Your GitHub org/user name. (Required)
     sortBy: 'updated', // stars | updated
-    limit: 10, // How many projects to display.
+    limit: 6, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
