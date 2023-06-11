f0440aab03ed1c99edaeeb5879bc05c034a1ca9e
Mo Zak <98157880+Zakops-com@users.noreply.github.com>
2023-06-11 13:21:48 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwind from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import tailwindConfig from './tailwind.config.mjs';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
