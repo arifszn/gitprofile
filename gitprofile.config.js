f83f8e0fd4ab009712c285ae85f691280b9797fc
Akhil <duvvaakhil.0@gmail.com>
2023-07-02 16:49:37 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -7,7 +7,7 @@ const config = {
     limit: 10, // How many projects to display.
     exclude: {
       forks: true, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['Committy'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
