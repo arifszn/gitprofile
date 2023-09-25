1bfc06be2ebadfe0fe47de1290068196096e3fe3
Tomer Klein <tomer.klein@gmail.com>
2023-09-25 09:51:26 +0300

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -131,7 +131,7 @@ const config = {
   blog: {
     source: 'medium', // medium | dev
     username: 'tomer.klein', // to hide blog section, keep it empty
-    limit: 3, // How many posts to display. Max is 10.
+    limit: 5, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
