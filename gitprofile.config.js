20929341dce0aef8db79a8f5eee7b95ed4ac1446
Rodgers Mogaka <47353893+rodgersxy@users.noreply.github.com>
2023-10-04 19:26:17 +0300

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'rodgersxy', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
