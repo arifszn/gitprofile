c5318da457b979e57a917e9d41d8c3b514c92415
Ariful Alam <ariful.dev@miaki.com.bd>
2024-01-20 18:28:08 +0600

    Remove App component

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import GitProfile from './components/gitprofile';
-
-function App() {
-  return <GitProfile config={CONFIG} />;
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import GitProfile from './components/gitprofile.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GitProfile config={CONFIG} />;
   </React.StrictMode>,
 );
