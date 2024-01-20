aa4c6928d6a170f14e0f47337974ed72c0c07fb6
Ariful Alam <ariful.dev@miaki.com.bd>
2024-01-20 18:24:26 +0600

    Migrate to TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import GitProfile from './components/gitprofile';
+
+function App() {
+  return <GitProfile config={CONFIG} />;
+}
+
+export default App;
---
+++ src/interfaces/article.tsx
@@ -0,0 +1,8 @@
+export interface Article {
+  title: string;
+  thumbnail: string;
+  link: string;
+  publishedAt: Date;
+  description: string;
+  categories: string[];
+}
