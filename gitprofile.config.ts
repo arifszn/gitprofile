ad45229c3f33ab31c214c2faa359d158093d7fed
Vishal Tewatia <tewatiavishal3@gmail.com>
2024-02-06 18:39:26 +0530

    customize it to my needs

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -10,30 +10,30 @@ const CONFIG = {
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
     external: {
-      header: 'My Projects',
-      // To hide the `External Projects` section, keep it empty.
-      projects: [
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-      ],
+      // header: 'My Projects',
+      // // To hide the `External Projects` section, keep it empty.
+      // projects: [
+      //   {
+      //     title: 'Project Name',
+      //     description:
+      //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+      //     imageUrl:
+      //       'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+      //     link: 'https://example.com',
+      //   },
+      //   {
+      //     title: 'Project Name',
+      //     description:
+      //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+      //     imageUrl:
+      //       'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+      //     link: 'https://example.com',
+      //   },
+      // ],
     },
   },
   seo: {
-    title: 'Portfolio of Ariful Alam',
+    title: 'Vishal Tewatia',
     description: '',
     imageURL: '',
   },
@@ -49,7 +49,7 @@ const CONFIG = {
     stackoverflow: '', // format: userid/username
     skype: '',
     telegram: '',
-    website: 'https://vishaltewatia.com',
+    website: 'https://blog.vishaltewatia.com',
     phone: '',
     email: 'tewatiavishal3@gmail.com',
   },
