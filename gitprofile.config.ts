2e52175c484c20507649e2d5312b48e121f02b4e
Nayoung Choi <152710037+iaaam0@users.noreply.github.com>
2024-02-26 05:07:06 +0900

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,7 +2,7 @@
 
 const CONFIG = {
   github: {
-    username: 'iaaam0', // Your GitHub org/user name. (This is the only required config)
+    username: 'iaaam0', // 내 깃허브 아이디
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
