08b4923d0f55e528f4c90a9a2dc25ac3e471231b
swazan.arif <swazan.arif@gmail.com>
2024-05-15 17:52:56 +0000

    Add support for Project IDX
    https://idx.dev/

---
+++ .idx/dev.nix
@@ -0,0 +1,24 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20
+  ];
+  idx.extensions = [];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
