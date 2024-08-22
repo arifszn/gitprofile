4e03ca5fb10e5ef8759eb7fad010b8eeb90b6b50
vishal tewatia <48011762+vishu42@users.noreply.github.com>
2024-08-22 13:02:31 +0530

    empty commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { VitePWA } from 'vite-plugin-pwa';
 import CONFIG from './gitprofile.config';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: CONFIG.base || '/',
