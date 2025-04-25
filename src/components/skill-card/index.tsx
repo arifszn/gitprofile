5e5fa9591d02b267d6f402cb7273cdccc8d60126
Kat Keilty <85399179+KatKeilty@users.noreply.github.com>
2025-04-25 19:32:01 -0400

    Update index.tsx
    changed "tech stack" to "skills"

--- src/components/skill-card/index.tsx
+++ src/components/skill-card/index.tsx
@@ -28,7 +28,7 @@ const SkillCard = ({
             {loading ? (
               skeleton({ widthCls: 'w-32', heightCls: 'h-8' })
             ) : (
-              <span className="text-base-content opacity-70">Tech Stack</span>
+              <span className="text-base-content opacity-70">Skills</span>
             )}
           </h5>
         </div>
