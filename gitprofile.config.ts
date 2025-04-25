70bd5e0924bc73a03c4a5ed0a1b7c5cbef3fc908
Kat Keilty <85399179+KatKeilty@users.noreply.github.com>
2025-04-25 19:19:58 -0400

    Update gitprofile.config.ts
    changed projects heading

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -29,7 +29,7 @@ const CONFIG = {
       },
     },
     external: {
-      header: 'My Experience & Projects',
+      header: 'Initiatives & Projects',
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
