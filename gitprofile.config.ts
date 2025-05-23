5dfa98d90ece14e446dc692a329030c5998ba382
ChristophShyper <45788587+ChristophShyper@users.noreply.github.com>
2025-05-23 22:19:37 +0200

    :zap: Trigger

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -19,7 +19,7 @@ const CONFIG = {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
         limit: 8, // How many projects to display.
         exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
+          forks: true, // Forked projects will not be displayed if set to true.
           projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
