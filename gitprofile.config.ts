b8460ac6cb75c16103d02d3688f368fba7a56c3f
William Green <20917127+wbgreen0405@users.noreply.github.com>
2025-06-09 14:19:24 -0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -246,13 +246,9 @@ const CONFIG = {
   },
 
   // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
+  footer: ``,
 
-  enablePWA: False,
+  enablePWA: True,
 };
 
 export default CONFIG;
